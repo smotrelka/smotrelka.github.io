b5f10518e30e47e703972e3c15ee2da29c5deace
balbesqueen <234616894+smotrelka@users.noreply.github.com>
2025-12-10 09:29:30 +0100

    metrica

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,6 +19,38 @@
 	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 	<meta name="apple-mobile-web-app-title" content="Смотрелка" />
 	<meta name="msapplication-TileColor" content="#6e11b0" />
+
+	<script type="text/javascript">
+		(function (m, e, t, r, i, k, a) {
+			m[i] =
+				m[i] ||
+				function () {
+					(m[i].a = m[i].a || []).push(arguments);
+				};
+			m[i].l = 1 * new Date();
+			for (var j = 0; j < document.scripts.length; j++) {
+				if (document.scripts[j].src === r) {
+					return;
+				}
+			}
+			((k = e.createElement(t)),
+				(a = e.getElementsByTagName(t)[0]),
+				(k.async = 1),
+				(k.src = r),
+				a.parentNode.insertBefore(k, a));
+		})(window, document, 'script', 'https://mc.webvisor.org/metrika/tag_ww.js', 'ym');
+
+		ym(101945774, 'init', { clickmap: true, accurateTrackBounce: true, trackLinks: true });
+	</script>
+	<noscript
+		><div>
+			<img
+				src="https://mc.yandex.ru/watch/101945774"
+				style="position:absolute; left:-9999px;"
+				alt=""
+			/>
+		</div></noscript
+	>
 </svelte:head>
 
 {@render children?.()}
